41eb4ddce2b83e58795e1d1f3fbd768aefcc98d3
VARUNKUMAR V <111338202+VARUNKUMAR2020@users.noreply.github.com>
2024-09-18 11:38:27 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,7 +24,7 @@ const About = () => {
           </p>
           <a
             rel="noreferrer"
-            href="https://drive.google.com/file/d/1BY0S0GS4IrUVtXmLEqqe51nUYRv2h7cq/view?usp=sharing"
+            href="https://drive.google.com/file/d/15Epl74735WcGKogsTRznGUP8XnQjJm8W/view?usp=sharing"
             target="_blank"
           >
             <button className="resume btn fw-bold fs-6">My Resume</button>
